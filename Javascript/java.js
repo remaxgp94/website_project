ec7ae2c80ff2617ec6cc3023e73853ecfd8d8c6b
RemaxGP <maksims.rjabcevs@yahoo.com>
2023-05-25 22:43:47 +0300

    Modification and significant updates in Javascipt code file

--- Javascript/java.js
+++ Javascript/java.js
@@ -1,9 +1,30 @@
-const websiteData = {
-userName:'Remax',
-reg:'26.12.1994',
-location:'Latvia',
-userFun: function() {
-    console.log(websiteData)
+//Website user data coding test
+
+const userName1 = {
+    nickname: 'RemaxGP',
+    regDate: '11.02.2022',
+    birthDate:'26.12.1994',
+    managed:true
+}
+const userName2 = {
+    nickname: 'Kirillus',
+    regDate: '17.03.2023',
+    birthDate: '25.05.1999',
+    managed:false
+}
+function managerCheck() {
+ if(userName1.managed === true) {
+    console.log('Congratulation! You are managed!')
+}
+else {
+    console.log('Do not supported')
 }
 }
-websiteData.userFun();
+function managerCheck2() {
+    if(userName2.managed === true) {
+       console.log('Congratulation! You are managed!')
+   }
+   else {
+       console.log('Do not supported')
+   }
+   }
